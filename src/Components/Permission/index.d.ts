0cd2b4d3e34141d5a0b669635891ee0f2b5e0eb5
Julio Cavallari <53496995+julio-cavallari@users.noreply.github.com>
2020-12-31 13:33:17 -0300

    Delete index.d.ts

--- src/Components/Permission/index.d.ts
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-import { PermissionProps } from "../../types";
-declare const Permission: ({ permissions, hasAny, has, children, }: React.PropsWithChildren<PermissionProps>) => JSX.Element;
-export default Permission;
